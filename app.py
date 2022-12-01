3d45ab74f0109776e40b63f2c277f148113ee007
David Luo <davidkluo@gmail.com>
2022-12-01 12:12:02 -0800

    Implement start page and question 1

--- app.py
+++ app.py
@@ -4,6 +4,24 @@ from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def show_survey_start():
+    """Generate survey start page with title, instructions, and a start button
+    and show the page"""
+    return render_template('survey_start.html', title=survey.title, instructions=survey.instructions)
+
+@app.post('/begin')
+def begin_survey():
+    """Start the survey by redirecting to the first question"""
+    return redirect('/questions/0')
+
+@app.get('/questions/<int:q_num>')
+def show_question(q_num):
+    """Generate and show form for current question"""
+    return render_template('question.html', question=survey.questions[q_num])
